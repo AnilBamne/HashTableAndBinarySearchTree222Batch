4faa8f55c100e656a5fbf2d2a3a26764586f7206
AnilBamne <anilbamne20@gmail.com>
2022-12-01 17:24:06 +0530

    Adding:Welcome message

---
+++ HashTableAndBinarySearchTree222Batch/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HashTableAndBinarySearchTree222Batch
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to HashTable And BinarySearchTree Programs");
+        }
+    }
+}
